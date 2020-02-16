9b86c4ff08b02cf182b5f104aded3134b8a94379
Paul Mackay <pauljmackay@gmail.com>
2020-02-16 13:16:56 +0000

    Add groups page, read data directly from Airtable
    More refinements to home page.
    Map partially working.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/files");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
   return {
---
+++ convert-groups.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+// import * as Papa from 'papaparse';
+// var Papa = require('papaparse');
+
+require('dotenv').config();
+const fs = require('fs');
+
+console.log('Converting groups CSV to JSON');
+
+// var data = Papa.parse('./src/_data/groups.csv');
+
+
+let Airtable = require('airtable')
+Airtable.configure({ apiKey: process.env.AIRTABLE_API_KEY });
+
+const base = Airtable.base(process.env.AIRTABLE_BASE_ABUNDANCE_NETWORK);
+
+let data = [];
+let  geojson = {
+  type: "FeatureCollection",
+  features: []
+};
+
+base('Groups').select({
+    view: 'Grid view'
+}).firstPage(function(err, records) {
+  if (err) { console.error(err); return; }
+  records.forEach(function(record) {
+    console.log('Retrieved', record.get('name'));
+    // console.log('Retrieved', record.fields);
+    // let group = JSON.parse(JSON.stringify(record.fields));
+    let group = JSON.parse(JSON.stringify(record.fields));
+    data.push(group);
+
+    let coords = record.fields.geolocation.split(',');
+    console.log(coords);
+    coords = [coords[1].trim(), coords[0].trim()];
+    console.log(coords);
+
+    let feature = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": coords
+      },
+      "properties": {
+        "name": record.fields.name,
+        'icon-image': 'poi-label'
+      }
+    };
+    geojson.features.push(feature);
+  });
+  let output = JSON.stringify(data);
+  console.log(output);
+  fs.writeFileSync('./src/_data/groups.json', output, 'utf-8'); 
+
+  let geojsonOutput = JSON.stringify(geojson);
+  console.log(geojsonOutput);
+  fs.writeFileSync('./src/files/groups.geojson', geojsonOutput, 'utf-8'); 
+});
+
+
+
